4efdf680195910a74e134ad715553e2f7dee6323
Lindsey Bell <lindseybell@snrenet.arizona.edu>
2025-02-19 09:23:26 -0700

    [minor] script setup

--- Code/FormatDailyData.R
+++ Code/FormatDailyData.R
@@ -1,2 +1,17 @@
 #Combine and format daily data
 
+#Load necessary packages
+library(dplyr)
+
+#Define file paths
+PathtoPre2022Data <- "./Data/pre2022data/DailyData_14022025.csv"
+PathtoPost2022Data <- "./Data/post2022data/DailyData_14022025.csv"
+
+PathtoOutput <- "./Data/Outputs/DailyData_19022025.csv"
+
+#Load data
+OldDat <- read.csv(PathtoPre2022Data)
+NewDat <- read.csv(PathtoPost2022Data)
+
+
+
